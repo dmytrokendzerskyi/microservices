586b3c02801e3b2b9fa6c8d55f50d0316b46edfd
Dmytro Kendzerskyi <dmytrokendzerskyi@gmail.com>
2018-12-25 18:28:43 +0200

    added custom health checking

--- price-client-service/src/main/java/com/pricepopulator/client/health/HealthCheck.java
+++ price-client-service/src/main/java/com/pricepopulator/client/health/HealthCheck.java
@@ -1,16 +1,27 @@
 package com.pricepopulator.client.health;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class HealthCheck implements HealthIndicator {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Value("${service.price.liveness}")
+    private String url;
+
     @Override
     public Health health() {
+        System.out.println("HEALTH CHECKING !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
         int errorCode = check();
-        if (errorCode != 0) {
+        if (errorCode != 200) {
             return Health.down()
                     .withDetail("Error Code", errorCode).build();
         }
@@ -18,6 +29,12 @@ public class HealthCheck implements HealthIndicator {
     }
 
     public int check() {
-        return 5001;
+        try {
+            return restTemplate.exchange(url, HttpMethod.GET,
+                    null, String.class).getStatusCodeValue();
+        }catch (Exception e){
+            e.printStackTrace();
+            return 0;
+        }
     }
 }
--- price-service/src/main/java/com/pricepopulator/repository/PriceRepositoryResourse.java
+++
@@ -1,9 +0,0 @@
-package com.pricepopulator.repository;
-
-import com.pricepopulator.model.Price;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "price" , path = "prices")
-public interface  PriceRepositoryResourse extends CrudRepository<Price, Long>{
-}
